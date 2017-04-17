ae44482072b12c7a8fd16db889ae62d9b443ec76
Mark Sherrington <thewayforward3000@gmail.com>
2017-04-17 18:23:24 +0100

    Seeds added for testing purposes

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
   belongs_to :user
   has_many :tickets
+  has_many :attendee_ids, class_name: "User", foreign_key: "user_id"
   attributes :id, :name, :location, :date, :description, :max_tickets, :tickets_left, :price, :image, :video, :attendee_ids
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,17 @@
   ActiveRecord::Base.connection.execute("TRUNCATE #{model.table_name} RESTART IDENTITY CASCADE")
 end
 
-mark = User.create!(username: "Mark", email: "look@me.com", image: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAg2AAAAJDUwMjk4YjI0LWI4MjUtNDkwNi04NWVmLTZlYWEzZWU1MzRiNg.jpg", bio: "Really cool guy")
+mark = User.create!(username: "Mark", email: "look@me.com", image: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAg2AAAAJDUwMjk4YjI0LWI4MjUtNDkwNi04NWVmLTZlYWEzZWU1MzRiNg.jpg", bio: "Legend")
 conor = User.create!(username: "Conor", email: "heen@slice", image: "", bio: "Irish Code Monkey")
 jack = User.create!(username: "Giacomo", email: "jack@mo", image: "", bio: "Italian Code Monkey")
 
 
-grad = Event.create!(name: "WDI25 Grad Ball", date: Date.new(2017, 4, 25), location: "Black Horse, Leman Street, London", user: mark, max_tickets: 8, tickets_left: 3, price: 0.00)
-pubbing = Event.create(name: "Piss-up", date: Date.new(2017, 4, 28), location: "A Brewery", max_tickets: 8, tickets_left: 7, price: 5.20, user: conor)
+grad = Event.create!(name: "WDI25 Grad Ball", location: "Black Horse Pub", date: Date.new(2017, 4, 25), description: "Massive knees up", max_tickets: 8, tickets_left: 3, price: 0.00, image: "", video: "", user: conor,  )
+organised = Event.create(name: "Piss-up", location: "A Brewery", date: Date.new(2017, 4, 28), description: "Piss-up in a brewery", max_tickets: 8, tickets_left: 7, price: 5.20, image: "", video: "", user: mark)
 
 Ticket.create!(event: grad, user: mark)
 Ticket.create!(event: grad, user: conor)
 Ticket.create!(event: grad, user: jack)
-Ticket.create!(event: pubbing, user: mark)
-Ticket.create!(event: pubbing, user: conor)
-Ticket.create!(event: pubbing, user: jack)
+Ticket.create!(event: organised, user: mark)
+Ticket.create!(event: organised, user: conor)
+Ticket.create!(event: organised, user: jack)
