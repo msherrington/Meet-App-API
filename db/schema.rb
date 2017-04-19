d0b5568541c63921969854656aaa188ebeb6f71b
Mark Sherrington <thewayforward3000@gmail.com>
2017-04-19 10:23:26 +0100

    Google places location autocomplete working and lat long added to Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418161314) do
+ActiveRecord::Schema.define(version: 20170419091738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20170418161314) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
