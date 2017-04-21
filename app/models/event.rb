8858600219db1ec016942f2f162c61dc097c2ba7
Conor <conor.heena2@mail.dcu.ie>
2017-04-21 14:40:45 +0100

    backend changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :tickets
-  has_many :comments
+  has_many :tickets, dependent: :nullify
+  has_many :comments, dependent: :destroy
   has_many :categories
   # has_and_belongs_to_many :attendees, class_name: "User", foreign_key: "user_id"
   has_many :users, through: :tickets
