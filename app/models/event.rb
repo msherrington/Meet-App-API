443a63ebbfd3d4bff90ab38a73c4a3fd43f221d0
Mark Sherrington <thewayforward3000@gmail.com>
2017-04-23 21:14:54 +0100

    minor change to email text files and seeds file

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,6 @@ class Event < ApplicationRecord
   has_many :tickets, dependent: :nullify
   has_many :comments, dependent: :destroy
   has_many :categories
-  # has_and_belongs_to_many :attendees, class_name: "User", foreign_key: "user_id"
   has_many :users, through: :tickets
   mount_uploader :image, ImageUploader
 end
