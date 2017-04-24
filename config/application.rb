5f2fa30becd700a4d4fb1a39ae8228d53b1207dd
Mark Sherrington <thewayforward3000@gmail.com>
2017-04-24 09:34:24 +0100

    setting up for deployment

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Project4
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins ['localhost:7000', 'https://sleepy-brushlands-92976.herokuapp.com/']
         resource '*', headers: :any, methods: :any
       end
     end
